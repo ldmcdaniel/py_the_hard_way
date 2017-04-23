149786be68ddde14c6ae123ffdf4297aba5939a5
Lucas McDaniel <lucas.mcdaniel@smiledirectclub.com>
2017-04-23 18:08:22 -0500

    Added spacing for old mcdonald

--- old_mcdonald.py
+++ old_mcdonald.py
@@ -1,17 +1,22 @@
 # from sys import argv
 
 # script, adjective, noun, animal, noise = argv
+eio = "Ee Aye Ee Aye Ohh"
 print "Give me a noun."
 noun = raw_input()
+print ""
 print "Give me an adjective."
 adjective = raw_input()
+print ""
 print "Give me an animal."
 animal = raw_input()
+print ""
 print "Give me a noise."
 noise = raw_input()
-
-eio = "Ee Aye Ee Aye Ohh"
-
+print ""
+print "===================="
+print "%s McDonald" %adjective
+print "===================="
 print "%s McDonald had a %s," % (adjective, noun)
 print  "%s." % (eio)
 print "And on that %s he had a %s, " % (noun, animal)
@@ -21,4 +26,4 @@ print "And a %s %s there." % (noise, noise)
 print "Here a %s, there a %s," % (noise, noise)
 print "Everywhere a %s %s," % (noise, noise)
 print "%s McDonald had a %s," % (adjective, noun)
-print  "%s." % (eio)
+print  "%s." %eio
